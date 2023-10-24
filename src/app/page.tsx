109595963a8eaa26f8f0bab118d1bbab84f150fd
Calin Dodon <calin.dodon@internal.ebs.md>
2023-10-24 17:54:34 +0300

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,9 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-         Home page
-        </p>
-       </div>
+      <p className={styles.homePage}>
+        [This is the home page]
+      </p>
     </main>
   )
 }
